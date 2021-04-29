5297340448f168005d131955201af1e98fa4853e
1999-kongyan <773809418@qq.com>
2021-04-29 15:43:25 +0800

    Initial commit

---
+++ app/src/main/java/com/example/zkt/MainActivity.java
@@ -0,0 +1,117 @@
+package com.example.zkt;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.RelativeLayout;
+
+import com.example.zkt.bean.Category;
+import com.example.zkt.bean.Goods;
+import com.example.zkt.ui.fragment.HomeFragment;
+import com.example.zkt.ui.fragment.LandFragment;
+import com.example.zkt.ui.fragment.MarketFragment;
+import com.example.zkt.ui.fragment.MineFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+import cn.leancloud.AVLogger;
+import cn.leancloud.AVOSCloud;
+import cn.leancloud.AVObject;
+import cn.leancloud.core.AVOSService;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private FragmentManager fragmentManager;
+    private RelativeLayout rl_content;
+    private int current_frag;
+    @BindView(R.id.bottomNavigationView)
+    BottomNavigationView bottomNavigationView;
+    Unbinder unbinder;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        fragmentManager = getSupportFragmentManager();
+        rl_content = findViewById(R.id.rl_content);
+
+        unbinder = ButterKnife.bind(this);
+
+        AVObject.registerSubclass(Category.class);
+        AVObject.registerSubclass(Goods.class);
+
+        initialize();
+    }
+
+
+    private void initialize() {
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);//设置 NavigationItemSelected 事件监听
+        //  BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);//改变 BottomNavigationView 默认的效果
+        //选中第一个item,对应第一个fragment
+        bottomNavigationView.setSelectedItemId(R.id.item_1);
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbinder.unbind();
+    }
+
+    //NavigationItemSelected 事件监听
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        changePageFragment(item.getItemId());
+        return true;
+    }
+
+    private void changePageFragment(int itemId) {
+        if(current_frag == itemId) return;//判断是否改变了界面
+        switch (itemId) {
+            case R.id.item_1: {
+                FragmentTransaction transaction = fragmentManager.beginTransaction();//创建一个事务
+                transaction.replace(R.id.rl_content, new HomeFragment());
+                current_frag = itemId;
+                bottomNavigationView.getMenu().getItem(0).setChecked(true);
+                transaction.commit();//事务一定要提交，replace才会有效
+                break;
+            }
+            case R.id.item_2: {
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.rl_content, new LandFragment());
+                current_frag = itemId;
+                bottomNavigationView.getMenu().getItem(1).setChecked(true);
+                transaction.commit();
+                break;
+            }
+            case R.id.item_3: {
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.rl_content, new MarketFragment());
+                current_frag = itemId;
+                bottomNavigationView.getMenu().getItem(2).setChecked(true);
+                transaction.commit();
+                break;
+            }
+            case R.id.item_4: {
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.rl_content, new MineFragment());
+                current_frag = itemId;
+                bottomNavigationView.getMenu().getItem(3).setChecked(true);
+                transaction.commit();
+                break;
+            }
+            default:
+                break;
+        }
+    }
+
+    //用于Fragment中跳转用
+    public void jumpPageFragment(int itemId){
+        changePageFragment(itemId);
+    }
+}
---
+++ app/src/main/java/com/example/zkt/activity/LoginActivity.java
@@ -0,0 +1,186 @@
+package com.example.zkt.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.text.InputType;
+import android.text.TextUtils;
+import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.zkt.MainActivity;
+import com.example.zkt.R;
+import com.example.zkt.bean.Category;
+import com.example.zkt.bean.Goods;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import cn.leancloud.AVLogger;
+import cn.leancloud.AVOSCloud;
+import cn.leancloud.AVObject;
+import cn.leancloud.AVUser;
+import cn.leancloud.core.AVOSService;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+
+import static cn.bmob.v3.Bmob.getApplicationContext;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private SharedPreferences loginPreference;
+
+
+    private EditText et_userPhone;
+    private EditText et_psw;
+    private TextView tv_register;
+    private Button btn_login;
+    private Switch sw_remember_pwd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AVOSCloud.setLogLevel(AVLogger.Level.DEBUG);
+
+        // 提供 this、App ID、App Key、Server Host 作为参数
+        // 注意这里千万不要调用 cn.leancloud.core.AVOSCloud 的 initialize 方法，否则会出现 NetworkOnMainThread 等错误。
+        AVOSCloud.initialize(this, "no7WRqvJp5Qvh0BwuPjxny98-gzGzoHsz", "rRGPQ4KgvdN0nFI4kvTNkqXW", "https://api.moliyunxuan.com");
+
+        setContentView(R.layout.activity_login);
+        initView();
+    }
+
+    private void initView() {
+        et_userPhone =(EditText) findViewById(R.id.met_userPhone);
+        et_psw = (EditText)findViewById(R.id.met_pwd);
+        btn_login = (Button) findViewById(R.id.btn_login);
+        tv_register = (TextView) findViewById(R.id.tv_register);
+        sw_remember_pwd = (Switch) findViewById(R.id.sw_remember_pwd);
+
+        //隐藏密码
+        et_psw.setTransformationMethod(PasswordTransformationMethod.getInstance());
+
+        //记住密码
+        loginPreference = getSharedPreferences("login", MODE_PRIVATE);
+        ///要通过loginPreference去记录三个参数（checked，userName，password）
+        boolean cheched = loginPreference.getBoolean("checked", false);
+        if (cheched) {
+            Map<String, Object> m = readLogin();
+            if (m != null) {
+                et_userPhone.setText((CharSequence) m.get("userName"));
+                et_psw.setText((CharSequence) m.get("password"));
+                sw_remember_pwd.setChecked(cheched);
+            }
+        }
+
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                configLoginInfo(sw_remember_pwd.isChecked());
+                login();
+            }
+        });
+        tv_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+                LoginActivity.this.finish();
+            }
+        });
+    }
+
+    /**
+     * 记住密码用
+     * 读登录信息
+     * @returnMap<String, Object>
+     */
+    public Map<String, Object> readLogin() {
+        Map<String, Object> m = new HashMap<>();
+        String userName = loginPreference.getString("userName", "");
+        String password = loginPreference.getString("password", "");
+        m.put("userName", userName);
+        m.put("password", password);
+        return m;
+    }
+
+    /**
+     * 记住密码用，用于记录保存的数据
+     * @param checked
+     */
+    public void configLoginInfo(boolean checked) {
+        SharedPreferences.Editor editor = loginPreference.edit();
+        editor.putBoolean("checked", sw_remember_pwd.isChecked());
+        if (checked) {
+            editor.putString("userName", et_userPhone.getText().toString());
+            editor.putString("password", et_psw.getText().toString());
+        } else {
+            editor.remove("userName").remove("password");
+        }
+        editor.commit();
+    }
+
+
+    private void login() {
+        final String userphone = et_userPhone.getText().toString();
+        final String password = et_psw.getText().toString();
+
+        // 校验手机号
+        if (TextUtils.isEmpty(userphone)) {
+            Toast.makeText(LoginActivity.this,"请输入手机号码",Toast.LENGTH_SHORT).show();
+            return;
+        } else if (userphone.length() != 11) {
+            Toast.makeText(LoginActivity.this,"请填写11位手机号",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (password.isEmpty()) {
+            Toast.makeText(LoginActivity.this,"请输入密码",Toast.LENGTH_LONG).show();
+            return;
+        }
+        AVUser.loginByMobilePhoneNumber(userphone,password).subscribe(new Observer<AVUser>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(AVUser avUser) {
+                // 登录成功
+                Log.d("hint", "登录成功 ");
+
+                LoginActivity.this.finish();
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                // 登录失败
+                Log.d("hint", "登录失败 ");
+                Toast.makeText(LoginActivity.this,e.getMessage(),Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
+
+    }
+
+
+
+
+
+
+
+}
